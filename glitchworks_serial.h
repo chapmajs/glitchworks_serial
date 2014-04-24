e66f9b4c2afb111e24d04a604cf821909f285a9f
Jonathan <chapmajs@gmail.com>
2014-04-24 15:21:33 -0400

    Adding GPLv3 notification to sources

--- glitchworks_serial.h
+++ glitchworks_serial.h
@@ -1,4 +1,20 @@
-/* A standard serial API for POSIX systems and MS-DOS */
+/*
+ * glitchworks_serial -- a library for serial communications for POSIX and DOS
+ * Copyright (C) 2014 Jonathan Chapman
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 int open_port(char *port, int speed);
 int close_port(char **port);
